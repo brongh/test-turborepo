6a8f4daac4db235a587c541b0867241615c31d51
bairong <brongh90@gmail.com>
2024-03-20 16:02:05 +0800

    added two apps

---
+++ apps/web2/next.config.mjs
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  transpilePackages: ["@repo/ui"],
+  basePath: "/web2",
+};
+
+export default nextConfig;
---
+++ packages/ui/src/web2.tsx
@@ -0,0 +1,8 @@
+export const Web2 = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div>
+      <h1>Web2 Component</h1>
+      {children}
+    </div>
+  );
+};
