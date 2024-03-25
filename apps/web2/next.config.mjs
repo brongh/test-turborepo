95bad17114debc379e757ba7cbc0b91c8d6fb9ca
bairong <brongh90@gmail.com>
2024-03-25 16:38:16 +0800

    next config beforeFile

--- apps/web2/next.config.mjs
+++ apps/web2/next.config.mjs
@@ -2,7 +2,13 @@
 const nextConfig = {
   transpilePackages: ["@repo/ui"],
   basePath: "/web2",
-  assetPrefix: "/web/",
+  assetPrefix: "https://test-turborepo-web2.vercel.app/web2",
+  beforeFiles: [
+    {
+      source: "/web2",
+      destination: "https://test-turborepo-web2.vercel.app/web2",
+    },
+  ],
 };
 
 export default nextConfig;
