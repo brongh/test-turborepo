bf87a155153b89dbd42fbdf51cc58a27e2e2fa09
bairong <brongh90@gmail.com>
2024-03-27 11:21:42 +0800

    add pages and navigation

--- apps/web2/src/app/first/page.tsx
+++ apps/web2/src/app/first/page.tsx
@@ -1,11 +1,11 @@
 "use client";
 import { useRouter } from "next/navigation";
 
-export default function Home() {
+export default function FirstPage() {
   const router = useRouter();
   return (
     <main className="min-h-screen w-full flex flex-col items-center justify-center">
-      <h1>Web2</h1>
+      <h1>Web2 - First Page</h1>
       <button
         onClick={(e: React.MouseEvent<HTMLButtonElement>) => {
           e.preventDefault();
